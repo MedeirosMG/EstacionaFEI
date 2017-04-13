590adb708b0aeb6020be1c26f6787f49d05069fb
Gabriel Medeiros <Gabriel Medeiros>
2017-04-13 17:43:03 -0300

    Teste

--- src/fei/estaciona/setor/Setor.java
+++ src/fei/estaciona/setor/Setor.java
@@ -3,6 +3,8 @@ package fei.estaciona.setor;
 public interface Setor 
 {	
 	public int vagasDisponiveis();
+
+
 	public void alterar_Disponibilidade_Setor(boolean disponivel);
 	public boolean Verifica_Disponibilidade_Setor();
 	public void alterar_Disponibilidade_Vaga(boolean disponivel);
