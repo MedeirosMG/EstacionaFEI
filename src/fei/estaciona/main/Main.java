ba9000dbee808b0199a3b0ac82053a03b111e209
Gabriel <gabriel.gomes@lighthouse.com.br>
2017-04-18 14:50:04 -0300

    Finalizacao molde setores

--- src/fei/estaciona/main/Main.java
+++ src/fei/estaciona/main/Main.java
@@ -1,10 +1,36 @@
 package fei.estaciona.main;
 
-import fei.estaciona.setor.*;
-import fei.estaciona.vaga.Vaga;
+import fei.estaciona.setor.SetorA;
 
 public class Main {
 	public static void main(String[] args)
 	{		
+		SetorA setor = new SetorA();
+		
+		setor.alterar_Disponibilidade_Setor(true);
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		
+		setor.alterar_Disponibilidade_Setor(false);
+		
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		setor.inserir_Nova_Vaga();
+		
+		int[] vetor = setor.vagas_Disponiveis();
+		
+		for (int i : vetor) {
+			if(i !=0)
+				System.out.println(i);
+			else
+				break;
+		}
 	}
 }
